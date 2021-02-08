577672f6543a84f0cf4f061044cbf94a6a36869a
robbailiff <robbailiff@gmail.com>
2021-02-08 22:57:40 +0000

    Added basic styling and button functionality

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+// Calculator //
+
+//Variables
+let displayValue = "";
+let operatorActive = false;
+
+// Selectors
+const display = document.querySelector('#input-display')
+
+// Listener Events
+
+const numberButtons = document.querySelectorAll('button.number')
+numberButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let pressedButton = e.target.id;
+
+        displayValue += pressedButton;
+        console.log(displayValue);
+        display.textContent = displayValue;
+    })
+});
+
+const operatorButtons = document.querySelectorAll('button.operation')
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let pressedButton = e.target.value;
+        
+        displayValue += ` ${pressedButton} `;
+        console.log(displayValue);
+        display.textContent = displayValue;
+    })
+});
+
+const functionButtons = document.querySelectorAll('button.function')
+functionButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let pressedButton = e.target.id;
+        window[pressedButton]();
+    })
+});
+
+// Operator functions
 
 function operate(operator, x, y) {
     return operator(x, y);
@@ -17,4 +59,28 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function back() {
+    console.log("The back button was pressed.")
+}
+
+function clear() {
+    console.log("The clear button was pressed.")
+}
+
+function percentage() {
+    console.log("The % button was pressed.")
+}
+
+function plusMinus() {
+    console.log("The +/- button was pressed.")
+}
+
+function decimal() {
+    console.log("The decimal button was pressed.")
+}
+
+function equals() {
+    console.log("The = button was pressed.")
 }
