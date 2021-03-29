897130c9b9109aa0d27be93fb7c11a098450dd0f
Rob Bailiff <robbailiff@gmail.com>
2021-03-29 15:49:52 +0100

    Tidied app.js and revamped logic for equals button

--- app.js
+++ app.js
@@ -1,63 +1,76 @@
-// Calculator //
+// =========
+// Variables
+// =========
 
-//Variables
 let displayValue = "";
 let operatorActive = false;
 
+// =========
 // Selectors
-const display = document.querySelector('#input-display')
+// =========
 
+const display = document.querySelector('#input-display');
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operation');
+const functionButtons = document.querySelectorAll('button.function');
+
+// ===============
 // Listener Events
+// ===============
 
-const numberButtons = document.querySelectorAll('button.number')
 numberButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
+
         let pressedButton = e.target.id;
 
         if (displayValue.length <= 1 && displayValue.charAt(0) == 0) {
             displayValue = pressedButton;
+
         } else {
             displayValue += pressedButton;
         }
-        console.log(displayValue);
         display.textContent = displayValue;
     })
 });
 
-const operatorButtons = document.querySelectorAll('button.operation')
 operatorButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
+
         let pressedButton = e.target.value;
-        
+
         if(!operatorActive) {
             displayValue += ` ${pressedButton} `;
-            console.log(displayValue);
             display.textContent = displayValue;
             operatorActive = true;
         }
     })
 });
 
-const functionButtons = document.querySelectorAll('button.function')
 functionButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
+
         let pressedButton = e.target.id;
 
         if(e.target.id == 'equals') {
-            console.log('Equals Button');
+
+            let displayArray = displayValue.split(' ');
+            let result = operate(displayArray[1], displayArray[0], displayArray[2]);
+
+            displayValue = String(Math.round(result * 1000) / 1000);
+            display.textContent = displayValue;
+            operatorActive = false;
+
         } else if (e.target.id == 'back' || e.target.id == 'clear') {
             window[pressedButton]();
+
         } else {
             let displayArray = displayValue.split(' ');
-
+            
             if (displayArray.length < 3) {
-                //change index 0
                 displayArray[0] = window[pressedButton](displayArray[0]);
-                console.log(displayArray[0])
+
             } else {
-                //change index 2
                 displayArray[2] = window[pressedButton](displayArray[2]);
-                console.log(displayArray[0])
         }
         displayValue = displayArray.join(' ');
         display.textContent = displayValue;
@@ -65,49 +78,69 @@ functionButtons.forEach((button) => {
     })
 });
 
-// Operator functions
+// ================
+// Button functions
+// ================
 
 function operate(operator, x, y) {
-    return operator(x, y);
+    x = Number(x);
+    y = Number(y);
+    
+    switch (operator) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+        case '^':
+            return exponent(x, y);
+        default:
+            return null;
+    }
 };
 
 function add(x, y) {
     return x + y;
-}
+};
 
 function subtract(x, y) {
     return x - y;
-}
+};
 
 function multiply(x, y) {
     return x * y;
-}
+};
 
 function divide(x, y) {
     return x / y;
-}
+};
 
 function exponent(x, y) {
     return x ** y;
-}
+};
 
 function back() {
     if (displayValue.charCodeAt(displayValue.length-1) == 32) {
         displayValue = displayValue.slice(0, -3);
         operatorActive = false;
+
     } else if (displayValue.length == 1) {
         displayValue = '0';
+        
     } else {
         displayValue = displayValue.slice(0, -1);
     }
     display.textContent = displayValue;
-}
+};
 
 function clear() {
     displayValue = '0';
     display.textContent = displayValue;
     operatorActive = false;
-}
+};
 
 function plusMinus(value) {
     if(value.charCodeAt(0) == 45){
@@ -115,7 +148,7 @@ function plusMinus(value) {
     } else {
         return '-' + value;
     }
-}
+};
 
 function decimal(value) {
     if(!value.includes('.')) {
@@ -123,30 +156,4 @@ function decimal(value) {
     } else {
         return value
     }
-}
-
-function equals() {
-    let operatorName = '';
-    const displayArray = displayValue.split(' ');
-    switch (displayArray[1]) {
-        case '+':
-            operatorName = 'add';
-            break;
-        case '-':
-            operatorName = 'subtract';
-            break;
-        case '*':
-            operatorName = 'multiply';
-            break;
-        case '/':
-            operatorName = 'divide';
-            break;
-        case '^':
-            operatorName = 'exponent';
-            break;
-        default:
-            return "Error"
-    }
-    const result = operate(operatorName, displayArray[0], displayArray[2])
-    console.log("The = button was pressed.")
-}
+};
